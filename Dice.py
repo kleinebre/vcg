3d5f52c2f95620b2fab4f31c849779c01d64acd9
Marc Brevoort <marc.brevoort@100percentit.com>
2023-04-12 20:38:47 +0100

    Two more ports and one edit

---
+++ Dice.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from random import random as rnd
+# Original apparently by Danny Freidus
+
+def title():
+    print((33 * " ") + "Number")
+    print((15 * " ") + "Creative Computing  Morristown, New Jersey")
+    print((15 * " ") + "Python version by Marc Brevoort")
+    print("")
+    print("")
+    print("")
+    print("This program simulates the rolling of a pair of dice.")
+    print("You enter the number of times you want the computer to")
+    print("'roll' the dice.  Watch out, very large numbers take")
+    print("a long time.  In particular, numbers over 5000.")
+    print("(Haha, modern hardware go brrrrr... how about 10 million)")
+
+def main():
+    while True:
+        total_spots = [0 for x in range(12)]
+        print("")
+        x = input("How many rolls? ")
+        for s in range(int(x)):
+            a = int(rnd()*6+1)
+            b = int(rnd()*6+1)
+            r = a + b
+            total_spots[r-1] += 1
+        print("")
+        print("Total spots | Number of times")
+        for v in range(1, 12):
+            print("{}      | {}".format(v + 1, total_spots[v]))
+        print("")
+        print("")
+        z = input("Try again? ")
+        if z == "y":
+            continue
+        break
+
+title()
+main()
---
+++ Number.py
@@ -0,0 +1,54 @@
+from random import random as rnd
+# A bit stupid game - ultimately you pretty much always win
+# by selecting the same number over and over again.
+def title():
+    print((33 * " ") + "Number")
+    print((15 * " ") + "Creative Computing  Morristown, New Jersey")
+    print((15 * " ") + "Python version by Marc Brevoort")
+    print("")
+    print("")
+    print("")
+    print("You have 100 points.  By guessing numbers from 1 to 5, you")
+    print("can gain or lose points depending upon how close you get to")
+    print("a random number selected by the computer.")
+    print("")
+    print("You occasionally will get a jackpot which will double(!)")
+    print("your point count.  You win when you get 500 points.")
+    print("")
+
+def fnr():
+    return int(5*rnd()+1)
+
+def main():
+    title()
+    p=100.0
+    while True:
+        g = input("Guess a number from 1 to 5: ")
+        r = fnr();
+        s = fnr();
+        t = fnr();
+        u = fnr();
+        v = fnr();
+        if g == r:
+            p -= 5
+        elif g == s:
+            p += 5
+        elif g == t:
+            p *= 2
+            print("You hit the jackpot!")
+        elif g == u:
+            p += 1
+        elif g == v:
+            p -= (p/2)
+        elif g > 5:
+            continue
+        else:
+            p -= 5
+
+        if p <= 500:
+            print("You have {} points.".format(p))
+            continue
+        print("!!!!YOU WIN!!!! With {} points.".format(p))
+        break
+
+main()
