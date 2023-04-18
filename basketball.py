bfaf2bed78f70e7bdb141dbc7d045c96e5c6f038
Marc Brevoort <kleinebre@hotmail.com>
2023-04-18 22:29:04 +0100

    Starting to look more like Python now

--- basketball.py
+++ basketball.py
@@ -1,141 +1,265 @@
-	PRINT TAB(31);"BASKETBALL"
-	PRINT TAB(15);"CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY"
-	PRINT:PRINT:PRINT
-	PRINT "THIS IS DARTMOUTH COLLEGE BASKETBALL.  YOU WILL BE DARTMOUTH"
-	PRINT " CAPTAIN AND PLAYMAKER.  CALL SHOTS AS FOLLOWS:  1. LONG"
-	PRINT " (30 FT.) JUMP SHOT; 2. SHORT (15 FT.) JUMP SHOT; 3. LAY"
-	PRINT " UP; 4. SET SHOT."
-	PRINT "BOTH TEAMS WILL USE THE SAME DEFENSE.  CALL DEFENSE AS"
-	PRINT "FOLLOWS:  6. PRESS; 6.5 MAN-TO MAN; 7. ZONE; 7.5 NONE."
-	PRINT "TO CHANGE DEFENSE, JUST TYPE 0 AS YOUR NEXT SHOT."
-	INPUT "YOUR STARTING DEFENSE WILL BE";D:IF D<6 THEN GOSUB 2010:PRINT: YS=1: GOTO center_jump
-	PRINT
-	INPUT "CHOOSE YOUR OPPONENT";O$
+#!/usr/bin/env python3
+from random import random as rnd
+US = 1
+THEM = 0
 
-###### MAIN LOOP
-center_jump:
-    IF YS=1 then YS=0: GOTO yourshot
-    PRINT "CENTER JUMP"
-	IF RND(1)> 3/5 THEN 420
-	PRINT O$;" CONTROLS THE TAP."
-	GOSUB 3000: GOTO center_jump
-420 PRINT "DARTMOUTH CONTROLS THE TAP."
-	PRINT
-yourshot:
-    INPUT "YOUR SHOT";Z
-	P=0
-	IF Z<>INT(Z) THEN PRINT "INCORRECT ANSWER.  RETYPE IT. ";:GOTO yourshot
-	IF Z<0 OR Z>4 THEN PRINT "INCORRECT ANSWER.  RETYPE IT. ";:GOTO yourshot
-	IF RND(1)<.5 THEN GOSUB 1000:GOTO center_jump
-	IF T<100 THEN GOSUB 1000: GOTO center_jump
-	PRINT
-	IF S(1)<>S(0) THEN PRINT "   ***** END OF GAME *****":PRINT "FINAL SCORE: DARTMOUTH:";S(1);"  ";O$;":";S(0):STOP
+LONG_JUMPSHOT = 1
+SHORT_JUMPSHOT = 2
+LAYUP = 3
+SETSHOT = 4
 
-	PRINT:PRINT "   ***** END OF SECOND HALF *****":PRINT
-	PRINT "SCORE AT END OF REGULATION TIME:"
-	PRINT "        DARTMOUTH:";S(1);"  ";O$;":";S(0)
-	PRINT
-	PRINT "BEGIN TWO MINUTE OVERTIME PERIOD"
-	T=93
-	GOTO center_jump
-###### END OF MAIN LOOP
-two_minutes_left:
-    PRINT
-	PRINT "   *** TWO MINUTES LEFT IN THE GAME ***"
-	PRINT
-	RETURN
+def title():
+    print(" " * 31 + "BASKETBALL")
+    print(" " * 15 + "CREATIVE COMPUTING MORRISTOWN, NEW JERSEY")
+    print("\n\n")
+    print("THIS IS DARTMOUTH COLLEGE BASKETBALL. YOU WILL BE DARTMOUTH")
+    print(" CAPTAIN AND PLAYMAKER. CALL SHOTS AS FOLLOWS: 1. LONG")
+    print(" (30 FT.) JUMP SHOT; 2. SHORT (15 FT.) JUMP SHOT; 3. LAY")
+    print(" UP; 4. SET SHOT.")
+    print("BOTH TEAMS WILL USE THE SAME DEFENSE. CALL DEFENSE AS")
+    print("FOLLOWS: 6. PRESS; 6.5 MAN-TO MAN; 7. ZONE; 7.5 NONE.")
+    print("TO CHANGE DEFENSE, JUST TYPE 0 AS YOUR NEXT SHOT.")
 
-1000 IF Z<>1 AND Z<>2 THEN GOSUB 1300: RETURN
-	T=T+1
-	IF T=50 THEN GOSUB end_of_first_half: RETURN
-	IF T=92 THEN GOSUB two_minutes_left
-	PRINT "JUMP SHOT"
-	IF RND(1)<=.341*D/8 THEN PRINT "SHOT IS GOOD.":GOSUB 7000:GOSUB 3000: RETURN
-	IF RND(1)>.682*D/8 THEN GOSUB 1200: RETURN
-	PRINT "SHOT IS OFF TARGET."
-	IF D/6*RND(1)>.45 THEN PRINT "REBOUND TO ";O$:GOSUB 3000: RETURN
-	PRINT "DARTMOUTH CONTROLS THE REBOUND."
-	IF RND(1)<=.4 THEN GOSUB 1300: RETURN
-	IF D<>6 OR RND(1) <= .6 THEN PRINT "BALL PASSED BACK TO YOU. "; :YS=1: RETURN
-	PRINT "PASS STOLEN BY ";O$;" EASY LAYUP.":GOSUB 6000:PRINT: YS=1: RETURN
 
-1200 IF RND(1)>.782*D/8 THEN
-        IF RND(1)>.843*D/8 THEN PRINT "CHARGING FOUL.  DARTMOUTH LOSES BALL.":GOSUB 3000: RETURN
-	    PRINT "SHOOTER IS FOULED.  TWO SHOTS.":GOSUB 4000:GOSUB 3000: RETURN
+def defensive_alignment():
+    while True:
+        defense = int(input("YOUR NEW DEFENSIVE ALIGNMENT IS "))
+        if defense >= 6:
+            return defense
+
+
+def get_starting_defense():
+    yourshot = 0
+    defense = int(input("YOUR STARTING DEFENSE WILL BE "))
+    if defense < 6:
+        defense = defensive_alignment()
+        print()
+        yourshot = 1
+    opponent = input("CHOOSE YOUR OPPONENT ")
+    # assume there is a label "center_jump" somewhere
+    return opponent, defense, yourshot
+
+
+def print_score(score):
+    print(f"SCORE: {score[US]} TO {score[THEM]}")
+
+
+def foul_shooting(score, p):
+    if defense <= 0.49:
+        print("SHOOTER MAKES BOTH SHOTS.")
+        score[1 - p] += 2
+        print_score(score)
+        return score
+    if rnd() <= 0.75:
+        print("BOTH SHOTS MISSED.")
+        return score
+    print("SHOOTER MAKES ONE SHOT AND MISSES ONE.")
+    score[1 - p] += 1
+    print_score(score)
+    return score
+
+
+def points_for_them(score):
+    score[THEM] += 2
+    print_score(score)
+    print()
+    return score
+
+
+def points_for_us(score):
+    score[THEM] += 2
+    print_score(score)
+    return score
+
+
+def end_of_first_half(score, opponent):
+    print("\n   ***** END OF FIRST HALF *****\n")
+    print(f"SCORE: DARTMOUTH: {score[US]}  {opponent}: {score[THEM]}\n\n")
+
+
+def two_minutes_left():
+    print("\n   *** TWO MINUTES LEFT IN THE GAME ***\n")
+
+
+def end_game(score, opponent):
+    print("   ***** END OF GAME *****")
+    print(f"FINAL SCORE: DARTMOUTH: {score[US]}  {opponent}: {score[THEM]}")
+
+
+def overtime(score, opponent):
+    print("\n   ***** END OF SECOND HALF *****\n")
+    print("SCORE AT END OF REGULATION TIME:")
+    print(f"        DARTMOUTH: {score[US]}  {opponent}: {score[THEM]}\n")
+    print("BEGIN TWO MINUTE OVERTIME PERIOD")
+
+
+def request_shot():
+    while True:
+        try:
+            shot = int(input("YOUR SHOT: "))
+            if shot < 0 or shot > 4:
+                print("INCORRECT ANSWER. RETYPE IT.")
+                continue
+            return shot
+        except ValueError:
+            print("INCORRECT ANSWER. RETYPE IT.")
+            continue
+
+
+def main():
+    title()
+    opponent, defense, yourshot = get_starting_defense()
+    game_time = 0
+    score = [0, 0]
+    while True:
+        if yourshot == 0:
+            print("CENTER JUMP")
+            if rnd() <= 3 / 5:
+                print(f"{opponent} CONTROLS THE TAP.")
+                game_time, score, yourshot = play_their_shot(game_time, score, player, opponent, defense)
+                continue
+            print("DARTMOUTH CONTROLS THE TAP.")
+            print()
+        shot_type = request_shot()
+        yourshot = 0
+        player = 0
+        if rnd() < 0.5:
+            game_time, score, yourshot = play_our_shot(shot_type, game_time, score, player, opponent, defense)
+            continue
+
+        if game_time < 100:
+            game_time, score, yourshot = play_our_shot(shot_type, game_time, score, player, opponent, defense)
+            continue
+
+        print()
+        if score[US] != score[THEM]:
+            end_of_game()
+            exit(0)
+        overtime(score, opponent)
+        game_time = 93
+        continue
+
+
+def play_our_shot(shot_type, game_time, score, player, opponent, defense):
+    yourshot = 0
+    if shot_type != LONG_JUMPSHOT and shot_type != SHORT_JUMPSHOT:
+        gosub_1300()
+        return game_time, score, yourshot
+    game_time += 1
+    if game_time == 50:
+        end_of_first_half(score, opponent)
+        return game_time, score, yourshot
+    if game_time == 92:
+        two_minutes_left()
+
+    print("JUMP SHOT")
+    if rnd() <= .341 * defense / 8:
+        print("SHOT IS GOOD.")
+        score = points_for_us(score)
+        game_time, score, yourshot = play_their_shot(game_time, score, player, opponent, defense)
+        return game_time, score, yourshot
+
+    if rnd() > .682 * defense / 8:
+        gosub_1200()
+        return game_time, score, yourshot
+
+    print("SHOT IS OFF TARGET.")
+    if defense / 6 * rnd() > .45:
+        print("REBOUND TO ", opponent)
+        game_time, score, yourshot = play_their_shot(game_time, score, player, opponent, defense)
+        return game_time, score, yourshot
+
+    print("DARTMOUTH CONTROLS THE REBOUND.")
+    if rnd() <= .4:
+        gosub_1300()
+        return game_time, score, yourshot
+
+    if defense != 6 or rnd() <= .6:
+        print("BALL PASSED BACK TO YOU.")
+        yourshot = 1
+        return game_time, score, yourshot
+
+    print(f"PASS STOLEN BY {opponent}, EASY LAYUP.")
+    score = points_for_them(score)
+    print()
+    yourshot = 1
+    return game_time, score, yourshot
+
+"""
+
+
+1200 IF rnd()>.782*defense /8 THEN
+        IF rnd()>.843*defense /8 THEN PRINT "CHARGING FOUL.  DARTMOUTH LOSES BALL.":game_time, score, yourshot = play_their_shot(game_time, score, player, opponent, defense): RETURN
+        PRINT "SHOOTER IS FOULED.  TWO SHOTS.":score = foul_shooting(score, p):game_time, score, yourshot = play_their_shot(game_time, score, player, opponent, defense): RETURN
     END IF
     PRINT "SHOT IS BLOCKED.  BALL CONTROLLED BY ";
-    IF RND(1)<=.5 THEN PRINT O$;".":GOSUB 3000: RETURN
+    IF rnd()<=.5 THEN
+		PRINT opponent;"."
+		game_time, score, yourshot = play_their_shot(game_time, score, player, opponent, defense)
+		RETURN game_time, score, yourshot
     PRINT "DARTMOUTH."
-    YS=1:
-	RETURN
+    yourshot=1
+    RETURN
 
 
-1300 T=T+1
-	IF T=50 THEN GOSUB end_of_first_half: RETURN
-	IF T=92 THEN GOSUB two_minutes_left
-	IF Z=0 THEN GOSUB 2010:PRINT: YS=1: RETURN
-	IF Z>3 THEN PRINT "SET SHOT." ELSE PRINT "LAY UP."
-	IF 7/D*RND(1)<=.4 THEN PRINT "SHOT IS GOOD.  TWO POINTS.":GOSUB 7000:GOSUB 3000: RETURN
-	IF 7/D*RND(1)>.7 THEN GOSUB 1500: RETURN
-	PRINT "SHOT IS OFF THE RIM."
-	IF RND(1)<=2/3 THEN PRINT O$;" CONTROLS THE REBOUND.":GOSUB 3000: RETURN
-	PRINT "DARTMOUTH CONTROLS THE REBOUND."
-	IF RND(1)<=.4 THEN GOTO 1300
-	PRINT "BALL PASSED BACK TO YOU.";
-	YS=1
-	RETURN
-
-1500 IF 7/D*RND(1)<=.875 THEN PRINT "SHOOTER FOULED.  TWO SHOTS.":GOSUB 4000:GOSUB 3000: RETURN
-	IF 7/D*RND(1)<=.925 THEN PRINT "SHOT BLOCKED. ";O$;"'S BALL.":GOSUB 3000: RETURN
-	PRINT "CHARGING FOUL.  DARTMOUTH LOSES THE BALL."
-	GOSUB 3000: RETURN
-
-2010 INPUT "YOUR NEW DEFENSIVE ALLIGNMENT IS";D
-	IF D<6 THEN 2010
-	RETURN
-
-3000 P=1
-	T=T+1
-	IF T=50 THEN GOSUB end_of_first_half: RETURN
-	PRINT
-	Z1=10/4*RND(1)+1
-	IF Z1>2 THEN 3500
-	PRINT "JUMP SHOT."
-	IF 8/D*RND(1)<=.35 THEN PRINT "SHOT IS GOOD.":GOSUB 6000:PRINT: YS=1: RETURN
-	IF 8/D*RND(1)>.75 THEN 3200
-	PRINT "SHOT IS OFF RIM."
-3110 IF D/6*RND(1)<=.5 THEN PRINT "DARTMOUTH CONTROLS THE REBOUND.":PRINT: YS=1: RETURN
-	PRINT O$;" CONTROLS THE REBOUND."
-	IF D<>6 THEN 3165
-    IF RND(1)<=.75 THEN PRINT "BALL STOLEN.  EASY LAY UP FOR DARTMOUTH.":GOSUB 7000: GOTO 3000
-3165 IF RND(1)<=.5 THEN PRINT "PASS BACK TO ";O$;" GUARD.":GOTO 3000
-	GOTO 3500
-3200 IF 8/D*RND(1)<=.9 THEN PRINT "PLAYER FOULED.  TWO SHOTS.":GOSUB 4000:PRINT: YS=1: RETURN
-	PRINT "OFFENSIVE FOUL.  DARTMOUTH'S BALL."
-	PRINT: YS=1: RETURN
-3500 IF Z1>3 THEN PRINT "SET SHOT." ELSE PRINT "LAY UP."
-	IF 7/D*RND(1)>.413 THEN PRINT "SHOT IS MISSED.":GOTO 3110
-	PRINT "SHOT IS GOOD."
-	GOSUB 6000
-	PRINT: YS=1: RETURN
-
-4000 REM FOUL SHOOTING
-	IF RND(1)<=.49 THEN PRINT "SHOOTER MAKES BOTH SHOTS.":S(1-P)=S(1-P)+2:GOSUB 7500:RETURN
-	IF RND(1)<=.75 THEN PRINT "BOTH SHOTS MISSED.": RETURN
-	PRINT "SHOOTER MAKES ONE SHOT AND MISSES ONE.":S(1-P)=S(1-P)+1:GOSUB 7500: RETURN
+1300 game_time += 1
+    if game_time == 50 THEN end_of_first_half(score, opponent): RETURN
+    if game_time == 92 THEN two_minutes_left()
+    IF shot_type=0 THEN GOSUB 2010:PRINT: yourshot=1: RETURN
+    IF shot_type>3 THEN PRINT "SET SHOT." ELSE PRINT "LAY UP."
+    IF defense*rnd()<=.4 THEN PRINT "SHOT IS GOOD.  TWO POINTS.":score = points_for_us(score):game_time, score, yourshot = play_their_shot(game_time, score, player, opponent, defense): RETURN
+    IF 7/ defense*rnd()>.7 THEN score = foul_handling(score, player, defense, opponent): RETURN
+    PRINT "SHOT IS OFF THE RIM."
+    IF rnd()<=2/3 THEN PRINT opponent;" CONTROLS THE REBOUND.":game_time, score, yourshot = play_their_shot(game_time, score, player, opponent, defense): RETURN
+    PRINT "DARTMOUTH CONTROLS THE REBOUND."
+    IF rnd()<=.4 THEN GOTO 1300
+    PRINT "BALL PASSED BACK TO YOU.";
+    yourshot=1
+    RETURN
+"""
 
 
-6000 S(0)=S(0)+2
-	GOSUB 7500
-	RETURN
-7000 S(1)=S(1)+2
-	GOSUB 7500
-	RETURN
-7500 PRINT "SCORE: ";S(1);"TO";S(0)
-	RETURN
-end_of_first_half:
-    PRINT:PRINT "   ***** END OF FIRST HALF *****":PRINT
-	PRINT "SCORE: DARTMOUTH:";S(1);"  ";O$;":";S(0)
-	PRINT
-	PRINT
-	RETURN
+def foul_handling(score, player, defense, opponent):
+    # TODO: Time!
+    if 7 / defense * rnd() <= 0.875:
+        print("SHOOTER FOULED. TWO SHOTS.")
+        score = foul_shooting(score, player)
+        run_3000(opponent)
+        return score
+    if 7 / defense * rnd() <= 0.925:
+        print(f"SHOT BLOCKED. {opponent}'S BALL.")
+        run_3000(opponent)
+        return score
+    print("CHARGING FOUL. DARTMOUTH LOSES THE BALL.")
+    run_3000(opponent)
+    return score
+
+
+"""
+3000 player=1
+    game_time += 1
+    if game_time == 50 THEN end_of_first_half(score, opponent): RETURN
+    PRINT
+    OPP_SHOTTYPE=10/4*rnd()+1
+    IF OPP_SHOTTYPE>2 THEN 3500
+    PRINT "JUMP SHOT."
+    IF 8/defense*rnd()<=.35 THEN PRINT "SHOT IS GOOD.":score = points_for_them(score):PRINT: yourshot=1: RETURN
+    IF 8/defense*rnd()>.75 THEN 3200
+    PRINT "SHOT IS OFF RIM."
+3110 IF defense /6*rnd()<=.5 THEN PRINT "DARTMOUTH CONTROLS THE REBOUND.":PRINT: yourshot=1: RETURN
+    PRINT opponent;" CONTROLS THE REBOUND."
+    IF defense<>6 THEN 3165
+    IF rnd()<=.75 THEN PRINT "BALL STOLEN.  EASY LAY UP FOR DARTMOUTH.":score = points_for_us(score): GOTO 3000
+3165 IF rnd()<=.5 THEN PRINT "PASS BACK TO ";opponent;" GUARD.":GOTO 3000
+    GOTO 3500
+3200 IF 8/defense*rnd()<=.9 THEN PRINT "PLAYER FOULED.  TWO SHOTS.":score = foul_shooting(score, p):PRINT: yourshot=1: RETURN
+    PRINT "OFFENSIVE FOUL.  DARTMOUTH'S BALL."
+    PRINT: yourshot=1: RETURN
+3500 IF OPP_SHOTTYPE>3 THEN PRINT "SET SHOT." ELSE PRINT "LAY UP."
+    IF 7/defense*rnd()>.413 THEN PRINT "SHOT IS MISSED.":GOTO 3110
+    PRINT "SHOT IS GOOD."
+    score = points_for_them(score)
+    PRINT: yourshot=1: RETURN
+"""
+
+main()
